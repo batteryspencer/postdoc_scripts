b819faa54d57eff7bd51d4d291f8b56276eddeba
Viswanath Pasumarthi <pasumarv@purdue.edu>
2024-03-21 14:39:35 -0400

    Implement data loading and processing from force statistics reports

--- postdoc_scripts/pmf_analysis.py
+++ postdoc_scripts/pmf_analysis.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import numpy as np
 
 # This function reads the force_stats_report.txt and extracts the values
@@ -9,3 +11,17 @@ def read_force_stats(file_path):
             md_steps = int(lines[2].split('=')[1].strip().split()[0])  # Extracts the number of MD steps
             return float(cv_data[0].strip()), -1*np.around(float(cv_data[1].strip()), 2), float(cv_data[2].strip()), md_steps
     return None
+
+# This dictionary will hold our data
+data = {'Constrained_Length': [], 'Mean_Force': [], 'Standard_Deviation': [], 'MD_Steps': []}
+
+# Assuming your directories are named in the '1.06_793' format and are in the current working directory
+for folder in glob.glob("[0-9].[0-9][0-9]_*"):
+    file_path = os.path.join(folder, 'force_stats_report.txt')
+    if os.path.isfile(file_path):
+        length, force, std_dev, md_steps = read_force_stats(file_path)
+        data['Constrained_Length'].append(length)
+        data['Mean_Force'].append(force)
+        data['Standard_Deviation'].append(std_dev)
+        data['MD_Steps'].append(md_steps)
+

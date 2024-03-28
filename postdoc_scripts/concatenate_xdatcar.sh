712a9c19e11aa6bb3d1d4f3220347e87d13dd7e8
Viswanath Pasumarthi <pasumarv@purdue.edu>
2024-03-28 07:57:57 -0400

    Refactor script to simplify directory pattern matching for segmented simulations

--- postdoc_scripts/concatenate_xdatcar.sh
+++ postdoc_scripts/concatenate_xdatcar.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# flag to suggest whether simulation is segmented or not
-SEGMENTED=true
-
-# if segmented
-if [ "$SEGMENTED" = true ]; then
-    # Directory pattern to match
-    DIRS=("01_0-0.5ps" "02_0.5-1.0ps" "03_1.0-1.204ps")
-else
-    # Directory pattern to match
-    DIR_PATTERN="RUN_??"
-fi
+# Directory pattern to match
+DIR_PATTERN="seg??"
 
 # Frame limit variable
 FRAME_LIMIT=5000
@@ -22,19 +13,9 @@ TEMP_FILE="temp_xdatcar"
 [ -f "$TEMP_FILE" ] && rm "$TEMP_FILE"
 
 # Loop through directories and concatenate XDATCAR files
-for dir in "${DIRS[@]}" $DIR_PATTERN; do
-    if [ -d "${dir}" ]; then
-        cat "${dir}/XDATCAR" >> "${TEMP_FILE}"
-    else
-        echo "Directory ${dir} does not exist"
-    fi
+for dir in $DIR_PATTERN; do
+    cat "${dir}/XDATCAR" >> "${TEMP_FILE}"
 done
-
-# Add the current directory's XDATCAR if not segmented
-if [ "$SEGMENTED" = false ]; then
-    # Add the current directory's XDATCAR
-    cat XDATCAR >> "$TEMP_FILE"
-fi
 
 # Extract the number of frames needed
 awk -v limit=$FRAME_LIMIT '/^Direct configuration/ {n++} n<=limit {print}' "$TEMP_FILE" > "XDATCAR_combined"

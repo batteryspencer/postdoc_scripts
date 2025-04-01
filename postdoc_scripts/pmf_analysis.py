Fix sign in free energy integration to correctly reflect ΔG = -∫F(x)dx.

postdoc_scripts/pmf_analysis.py
@@ -174,17 +174,21 @@ def calculate_pmf(x, y, std_dev):
 
     # Integrate force (using Simpson's rule) to estimate free energy barriers
     # Forward barrier: from IS to TS
-    forward_barrier = abs(simps(y_sorted[idx_is:idx_ts + 1], x_sorted[idx_is:idx_ts + 1]))
+    forward_barrier = abs(-simps(y_sorted[idx_is:idx_ts + 1], x_sorted[idx_is:idx_ts + 1]))
     forward_variance = simps((std_sorted[idx_is:idx_ts + 1])**2, x_sorted[idx_is:idx_ts + 1])
     forward_std = np.sqrt(forward_variance)
     
     # Backward barrier: from TS to FS
-    backward_barrier = abs(simps(y_sorted[idx_ts:idx_fs + 1], x_sorted[idx_ts:idx_fs + 1]))
+    backward_barrier = abs(-simps(y_sorted[idx_ts:idx_fs + 1], x_sorted[idx_ts:idx_fs + 1]))
     backward_variance = simps((std_sorted[idx_ts:idx_fs + 1])**2, x_sorted[idx_ts:idx_fs + 1])
     backward_std = np.sqrt(backward_variance)
 
     # Compute free energy of reaction (ΔG) and its uncertainty from IS to FS
-    delta_G = abs(simps(y_sorted[idx_is:idx_fs + 1], x_sorted[idx_is:idx_fs + 1]))
+    # ΔG = -∫F(x)dx from IS to FS
+    # Note: The negative sign is used to convert from force to free energy
+    # The integral of the force gives the change in free energy
+    # between the initial and final states.
+    delta_G = -simps(y_sorted[idx_is:idx_fs + 1], x_sorted[idx_is:idx_fs + 1])
     delta_G_var = simps((std_sorted[idx_is:idx_fs + 1])**2, x_sorted[idx_is:idx_fs + 1])
     delta_G_std = np.sqrt(delta_G_var)
 